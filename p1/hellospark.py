27cd5d6a7d179301d462ecd2f030ea9e3f953320
ans-web07 <07anshika@gmail.com>
2024-12-27 18:22:58 +0530

    Pyspark 1st app
    Signed-off-by: ans-web07 <07anshika@gmail.com>

--- p1/hellospark.py
+++ p1/hellospark.py
@@ -1,15 +1,23 @@
-
-
+from pyspark import SparkConf
+from pyspark.shell import spark
 from pyspark.sql import *
 from lib.logger import Log4J
 #local multithreaded JVM with 3 threads    = local[3] below
 #app_name in logger.py goes from appName set here
 if __name__ == "__main__":
-    #obtaining a spark session
-    spark = SparkSession.builder\
+    #obtaining a spark session and setting configurations
+    '''spark = SparkSession.builder\
             .appName("Hello Spark")\
             .master("local[3]")\
-            .getOrCreate()
+            .getOrCreate()'''
+
+    #Another way for configuring spark session
+    config = SparkConf()
+    config.set("spark.app.name","Hello Spark")
+    config.set("spark.master","local[3]")
+    spark = SparkSession.builder\
+            .config(conf=config)\
+    .getOrCreate()
 
     logger = Log4J(spark)
     logger.info("Starting hellospark")
